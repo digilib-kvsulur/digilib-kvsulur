b1b2bba93add34d6c548943f51b3296f529fe954
Tanish Vettrivel G V <tanishvettrivel2010@gmail.com>
2026-09-20 10:55:50 +0530

    Update index.ts

--- supabase/functions/push-notification/index.ts
+++ supabase/functions/push-notification/index.ts
@@ -16,7 +16,7 @@ serve(async (req) => {
   }
 
   try {
-    const serviceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || Deno.env.get('SUPABASE_ANON_KEY') ?? ''
+    const serviceRoleKey = (Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || Deno.env.get('SUPABASE_ANON_KEY')) ?? ''
     const supabaseClient = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
       serviceRoleKey
